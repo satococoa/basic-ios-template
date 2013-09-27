e59a81b7f83fb47e1f870971cd8c010be604cdc1
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-09-28 02:08:10 +0900

    Add startup code with Storyboard

--- files/app/app_delegate.rb
+++ files/app/app_delegate.rb
@@ -1,6 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    return true if RUBYMOTION_ENV == 'test'
     @window = UIWindow.alloc.initWithFrame(App.bounds)
+    storyboard = UIStoryboard.storyboardWithName('Storyboard', bundle: nil)
+    @window.rootViewController = storyboard.instantiateInitialViewController
     @window.makeKeyAndVisible
     true
   end
